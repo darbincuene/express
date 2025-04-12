4453e21555ce32a0cf1622954347a12435ad9b3a
Darbin cuene <celedon22cuene@gmail.com>
2025-04-12 01:32:37 -0500

    cambios

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import connectDB from "./src/config/db.js";
 import {router} from "./src/routes/rutas.js";
+import cors from "cors";
 
 const app=  express();
 const PORT=12345;
 
 connectDB();
 
+app.use(cors({}));
 app.use(express.json());
 app.use(router);
 
